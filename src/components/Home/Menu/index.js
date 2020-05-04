56b093e4b295ad4927667bcffc1af10d58e816b3
adeiltonsousa <adeiltonsousaxavier@gmail.com.br>
2020-05-04 20:12:33 -0300

    ajuste do Menu Buguer

--- src/components/Home/Menu/index.js
+++ src/components/Home/Menu/index.js
@@ -14,18 +14,11 @@ class MenuBurger extends React.Component {
       return (
           <>
             <span className="icone-menu">
-                <Menu>
-                    <a id="home" className="menu-item" href="/">Minha Conta</a>
-                    <a id="about" className="menu-item" href="/about">Meu Rank</a>
-                    <a id="contact" className="menu-item" href="/contact">Minhas Metas</a>
-                    <a id="configuracao" className="menu-item" href="/contact">Configuração</a>
-                    <a id="ajuda" className="menu-item" href="/ajuda">Ajuda</a>
-                    <a onClick={ this.showSettings } className="menu-item--small" href="/home">Settings</a>
-                </Menu>
-                    <img className="icone_menu_svg" src={icone} alt="Icone de Menu"/>
+                    <img src={icone} alt="Icone de Menu"/>
             </span>
-          <div className="container-menu">
-          </div>
+          <Menu>
+             
+          </Menu>
           </>
       );
     }
